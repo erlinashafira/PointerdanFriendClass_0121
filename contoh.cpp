4f8abf1d1177576fbc5b1be7293b092a3c1010c4
errerew <erlina31acc@gmail.com>
2025-05-06 18:28:25 +0700

    menghitung menggunakan pointer pada luas

---
+++ contoh.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Persegi_Panjang
+{
+    public: //akses modifier
+    int panjang, lebar;
+
+    public:
+    void input() {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkan lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+    //Lingkaran ol;
+};
